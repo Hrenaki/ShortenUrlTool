2c45db20851583eac61fc3288011f4097bbe5718
Hrenaki <shikhovwtr@mail.ru>
2021-09-17 20:31:15 +0700

    Added url db context

---
+++ Data/UrlDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class UrlDbContext : DbContext
+    {
+        public DbSet<UrlEntity> Urls { get; set; }
+
+        public UrlDbContext(DbContextOptions options) : base(options)
+        { }
+    }
+}
---
+++ Data/UrlEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class UrlEntity : Entity
+    {
+        public string LongUrl { get; set; }
+        public string ShortUrlRelativePath { get; set; }
+    }
+}
