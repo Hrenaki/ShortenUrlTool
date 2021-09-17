03c7a401ef8aa9c30eb83a660c77c11eaa22659b
Hrenaki <shikhovwtr@mail.ru>
2021-09-17 22:21:14 +0700

    Added shorten url service

---
+++ Core/Services/IShortenUrlService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Services
+{
+    public interface IShortenUrlService
+    {
+        string CreateShortRelativeUrl(string longUrl);
+        string GetLongUrlFromShortRelativeUrl(string shortRelativeUrl);
+    }
+}
---
+++ Core/Services/ShortenUrlService.cs
@@ -0,0 +1,52 @@
+﻿using Data;
+using LiteX.Guard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Services
+{
+    public class ShortenUrlService : IShortenUrlService
+    {
+        private UrlDbContext _urlDbContext;
+
+        public ShortenUrlService(UrlDbContext urlDbContext)
+        {
+            Guard.NotNull(urlDbContext, nameof(urlDbContext));
+
+            _urlDbContext = urlDbContext;
+        }
+
+        public string CreateShortRelativeUrl(string longUrl)
+        {
+            var urlEntity = new UrlEntity() { LongUrl = longUrl };
+            _urlDbContext.Urls.Add(urlEntity);
+
+            var shortRelativeUrl = GetByteString(urlEntity.Id);
+            urlEntity.ShortRelativeUrl = shortRelativeUrl;
+
+            _urlDbContext.Update(urlEntity);
+
+            _urlDbContext.SaveChanges();
+
+            return shortRelativeUrl;
+        }
+
+        public string GetLongUrlFromShortRelativeUrl(string shortRelativeUrl)
+        {
+            var urlEntity = _urlDbContext.Urls.SingleOrDefault(entity => string.Equals(entity.ShortRelativeUrl, shortRelativeUrl));
+
+            return urlEntity != null ? urlEntity.LongUrl : null;
+        }
+
+        private string GetByteString(int number)
+        {
+            var bytes = BitConverter.GetBytes(number);
+
+            var result = Convert.ToBase64String(bytes);
+            return result;
+        }
+    }
+}
