3c93956a3a60cdd3b3ff6f7e50e8e04acbf7d0c5
Hrenaki <shikhovwtr@mail.ru>
2021-09-29 11:51:21 +0700

    Added HomeController

--- ShortenUrlWeb/Controllers/HomeController.cs
+++ ShortenUrlWeb/Controllers/HomeController.cs
@@ -1,18 +1,73 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Core.Services;
+using LiteX.Guard;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Controllers;
+using Microsoft.AspNetCore.Mvc.Routing;
+using ShortenUrlWeb.Models.Home;
 
 namespace ShortenUrlWeb.Controllers
 {
     [Route("/home")]
     public class HomeController : Controller
     {
+        private IShortenUrlService _shortenUrlService;
+
+        public HomeController(IShortenUrlService shortenUrlService)
+        {
+            Guard.NotNull(shortenUrlService, nameof(shortenUrlService));
+
+            _shortenUrlService = shortenUrlService;
+        }
+
         [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult GetShortenUrl(string longUrl)
+        {
+            var result = new Result();
+
+            if (string.IsNullOrEmpty(longUrl))
+            {
+                result.IsSuccessful = false;
+                result.Message = "Long url is empty!";
+
+                return PartialView("Result", result);
+            }
+
+            var shortRelativeUrl = _shortenUrlService.CreateShortRelativeUrl(longUrl);
+            if(shortRelativeUrl == null)
+            {
+                result.IsSuccessful = false;
+                result.Message = "Can't create short url!";
+
+                return PartialView("Result", result);
+            }
+
+            var actionName = "Index";
+            var controllerName = "Redirect";
+            var values = new { shortRelativeUrl = shortRelativeUrl };
+
+            var url = this.Url.Action(actionName, controllerName, values, Request.Scheme);
+            if(url == null)
+            {
+                result.IsSuccessful = false;
+                result.Message = "Can't create url to redirect tool!";
+
+                return PartialView("Result", result);
+            }
+
+            result.IsSuccessful = true;
+            result.InternalUrl = url;
+
+            return PartialView("Result", result);
         }
     }
 }
---
+++ ShortenUrlWeb/Models/Home/Result.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShortenUrlWeb.Models.Home
+{
+    public class Result
+    {
+        public bool IsSuccessful { get; set; }
+        public string InternalUrl { get; set; }
+        public string Message { get; set; }
+    }
+}
