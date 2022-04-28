b42847bfac08c13452f2136ec79fafa000cd9f50
Hrenaki <shikhovwtr@mail.ru>
2022-04-28 11:39:32 +0700

    ShortenUrlDesktop development was started

---
+++ ShortenUrlDesktop/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ShortenUrlDesktop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static DependencyProperty ViewModelProperty;
+
+        internal ShortenUrlViewModel ViewModel
+        {
+            get => (ShortenUrlViewModel)GetValue(ViewModelProperty);
+            private set => SetValue(ViewModelProperty, value);
+        }
+
+        static MainWindow()
+        {
+            ViewModelProperty = DependencyProperty.Register(nameof(ViewModel), typeof(ShortenUrlViewModel), typeof(MainWindow));
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ShortenUrlDesktop/ShortenUrlViewModel.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using APICommunication;
+
+namespace ShortenUrlDesktop
+{
+    internal class ShortenUrlViewModel : INotifyPropertyChanged
+    {
+        private IAPIClient client;
+
+        #region Properties
+        private string longUrl;
+        public string LongUrl
+        {
+            get => longUrl;
+            set
+            {
+                if(!string.IsNullOrWhiteSpace(value) && value != longUrl)
+                {
+                    longUrl = value;
+                    OnPropertyChanged(nameof(LongUrl));
+                }
+            }
+        }
+
+        private string shortUrl;
+        public string ShortUrl
+        {
+            get => shortUrl;
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value) && value != shortUrl)
+                {
+                    shortUrl = value;
+                    OnPropertyChanged(nameof(ShortUrl));
+                }
+            }
+        }
+        #endregion
+
+        #region Commands
+        private RelayCommand getShortUrlCommand;
+        public RelayCommand GetShortUrlCommand
+        {
+            get => getShortUrlCommand ?? (getShortUrlCommand = new RelayCommand(obj => GetShortUrl()));
+        }
+
+        private RelayCommand getLongUrlByShortCommand;
+        public RelayCommand GetLongUrlByShortCommand
+        {
+            get => getLongUrlByShortCommand ?? (getLongUrlByShortCommand = new RelayCommand(obj => GetLongUrlByShort()));
+        }
+        #endregion
+
+        public ShortenUrlViewModel()
+        {
+            client = APIClientFactory.CreateDefault();
+        }
+
+        public async void GetShortUrl()
+        {
+            ShortUrl = await client.GetAbsoluteShortURLAsync(LongUrl);
+        }
+
+        public async void GetLongUrlByShort()
+        {
+            LongUrl = await client.GetOriginURLByAbsoluteShortURL(shortUrl);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
